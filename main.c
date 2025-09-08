cbb0997660a9e87b2b7ff3e9e7cb827a61defd87
bcuixart <bernatcuixart@gmail.com>
2025-09-08 20:38:21 +0200

    Added automatic image size generator

--- main.c
+++ main.c
@@ -27,9 +27,16 @@ int main(int argc, char** argv)
 	if (read_image(argv[1], &read_image_data) == -1) error_and_exit("Loading image", 7);
 
 	struct image_data smaller_image_data;
-	if (get_smaller_image_data(&read_image_data, &smaller_image_data, 10, 10) == -1) error_and_exit("Making smaller image", 8);
-	if (save_image_as_ppm(&smaller_image_data, "output/Output.ppm") == -1) error_and_exit("Saving smaller image", 9);
+	char buff[128];
+	for (int i = 0; i < 10; ++i)
+	{
+		int power_of_two = (1 << i);
+		sprintf(buff, "output/Output%d.ppm", i);
 
-	free(smaller_image_data.pixel_rgb_matrix);
+		if (get_smaller_image_data(&read_image_data, &smaller_image_data, power_of_two, power_of_two) == -1) error_and_exit("Making smaller image", 8);
+		if (save_image_as_ppm(&smaller_image_data, buff) == -1) error_and_exit("Saving smaller image", 9);
+		free(smaller_image_data.pixel_rgb_matrix);
+	}
+
 	free(read_image_data.pixel_rgb_matrix);
 }
