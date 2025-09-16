7651642540132305de39cb734e6e4ad8c1d0ce47
bcuixart <bernatcuixart@gmail.com>
2025-09-16 23:10:05 +0200

    Game in playable state

--- game.c
+++ game.c
@@ -28,6 +28,122 @@ int should_close_game_window()
 
 void update_game()
 {
+	int screen_width = GetScreenWidth();
+	int screen_height = GetScreenHeight();
+
+	BeginDrawing();
+	ClearBackground(RAYWHITE);
+
+	BeginMode2D(camera);
+
+	camera.target = (Vector2){ 128, 128 };
+	camera.offset = (Vector2){ (float)screen_width / 2, (float)screen_height / 2 };
+	camera.rotation = 0.0f;
+	camera.zoom = 0.6666f * min(screen_width, screen_height) / 256.f;
+
+	if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
+	{
+		Vector2 mouse_pos = GetMousePosition();
+		Vector2 mouse_world = GetScreenToWorld2D(mouse_pos, camera);
+
+		struct game_image_node* list_iterator = nodes_list_first;
+		while (list_iterator != NULL)
+		{
+			float dx = mouse_world.x - list_iterator->position.x;
+			float dy = mouse_world.y - list_iterator->position.y;
+			if (sqrtf(dx * dx + dy * dy) <= list_iterator->radius)
+			{
+				click_node(list_iterator);
+				break;
+			}
+
+			list_iterator = list_iterator->next;
+		}
+	}
+
+	struct game_image_node* list_iterator = nodes_list_first;
+	while (list_iterator != NULL)
+	{
+		DrawCircleV(list_iterator->position, list_iterator->radius, list_iterator->color);
+
+		list_iterator = list_iterator->next;
+	}
+
+	EndMode2D();
+	EndDrawing();
+}
+
+void click_node(struct game_image_node* node)
+{
+	if (node->level >= game_smaller_images_level - 1) return;
+
+	int new_level = node->level + 1;
+	int new_level_color_matrix_width = (1 << new_level);
+	int new_radius = node->radius / 2;
+
+	struct game_image_node* n1 = malloc(sizeof(struct game_image_node));
+	struct game_image_node* n2 = malloc(sizeof(struct game_image_node));
+	struct game_image_node* n3 = malloc(sizeof(struct game_image_node));
+	//if (!n1 || !n2 || !n3) return -1;
+
+	Vector2 pos0 = (Vector2){ node->position.x + new_radius, node->position.y + new_radius };
+	Vector2 pos1 = (Vector2){ node->position.x - new_radius, node->position.y + new_radius };
+	Vector2 pos2 = (Vector2){ node->position.x + new_radius, node->position.y - new_radius };
+	Vector2 pos3 = (Vector2){ node->position.x - new_radius, node->position.y - new_radius };
+
+	int i0 = (int)((pos0.x / 256) * new_level_color_matrix_width);
+	int j0 = (int)((pos0.y / 256) * new_level_color_matrix_width);
+	int i1 = (int)((pos1.x / 256) * new_level_color_matrix_width);
+	int j1 = (int)((pos1.y / 256) * new_level_color_matrix_width);
+	int i2 = (int)((pos2.x / 256) * new_level_color_matrix_width);
+	int j2 = (int)((pos2.y / 256) * new_level_color_matrix_width);
+	int i3 = (int)((pos3.x / 256) * new_level_color_matrix_width);
+	int j3 = (int)((pos3.y / 256) * new_level_color_matrix_width);
+
+	int idx0 = j0 * new_level_color_matrix_width + i0;
+	int idx1 = j1 * new_level_color_matrix_width + i1;
+	int idx2 = j2 * new_level_color_matrix_width + i2;
+	int idx3 = j3 * new_level_color_matrix_width + i3;
+
+	Color color0 = (Color){ game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx0].r,
+		game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx0].g,
+		game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx0].b, 255
+	};
+	Color color1 = (Color) { game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx1].r,
+		game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx1].g,
+		game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx1].b, 255
+	};
+	Color color2 = (Color) { game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx2].r,
+		game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx2].g,
+		game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx2].b, 255
+	};
+	Color color3 = (Color) { game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx3].r,
+		game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx3].g,
+		game_smaller_images_matrix[new_level].pixel_rgb_matrix[idx3].b, 255
+	};
+
+	set_node_values(n1, color0, pos0, new_level, new_radius);
+	set_node_values(n2, color1, pos1, new_level, new_radius);
+	set_node_values(n3, color2, pos2, new_level, new_radius);
+	set_node_values(node, color3, pos3, new_level, new_radius);
+
+	n3->next = node->next;
+	node->next = n1;
+	n1->next = n2;
+	n2->next = n3;
+}
+
+void set_node_values(struct game_image_node* node, Color color, Vector2 position, int level, int radius)
+{
+	node->position = position;
+	node->level = level;
+	node->radius = radius;
+	node->color = color;
+}
+
+/*
+void update_game()
+{
 	if (!game_smaller_images_matrix) return;
 
 	int screen_width = GetScreenWidth();
@@ -56,6 +172,7 @@ void update_game()
 
 	Color color;
 	color.a = 255;
+
 	for (int i = 0; i < image_height; ++i)
 	{
 		for (int j = 0; j < image_width; ++j)
@@ -76,6 +193,7 @@ void update_game()
 
 	EndDrawing();
 }
+*/
 
 void load_image_matrix(struct image_data* img_mtrx, int level)
 {
@@ -83,4 +201,13 @@ void load_image_matrix(struct image_data* img_mtrx, int level)
 	game_smaller_images_level = level;
 
 	game_current_level = 0;
+
+	nodes_list_first = malloc(sizeof(struct game_image_node));
+	//if (!nodes_list_first) { perror("malloc"); exit(1); }
+
+	nodes_list_first->level = 0;
+	nodes_list_first->radius = 128;
+	nodes_list_first->color = (Color){ img_mtrx[0].pixel_rgb_matrix[0].r,img_mtrx[0].pixel_rgb_matrix[0].g, img_mtrx[0].pixel_rgb_matrix[0].b, 255 };
+	nodes_list_first->position = (Vector2){ 128, 128 };
+	nodes_list_first->next = NULL;
 }
--- game.h
+++ game.h
@@ -17,16 +17,33 @@
 
 #define GAME_LEVELS 8
 
+struct game_image_node 
+{
+	Color color;
+	Vector2 position;
+	int level;
+	int radius;
+
+	struct game_image_node* next;
+};
+
 void init_game_window();
 void close_game_window();
 int should_close_game_window();
 
 void update_game();
 
+void click_node(struct game_image_node* node);
+void set_node_values(struct game_image_node* node, Color color, Vector2 position, int level, int radius);
+
 void load_image_matrix(struct image_data* img_mtrx, int level);
+
+struct game_image_node* nodes_list_first;
 
 extern struct image_data* game_smaller_images_matrix;
 extern int game_smaller_images_level;
 extern int game_current_level;
+
+Camera2D camera;
 
 #endif
